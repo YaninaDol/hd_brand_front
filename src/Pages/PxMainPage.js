0378e8c62778c96477b9e0c58b4ed52545dea606
Yana <95874369+YaninaDol@users.noreply.github.com>
2024-02-26 13:35:31 +0400

    search field

--- src/Pages/PxMainPage.js
+++ src/Pages/PxMainPage.js
@@ -665,14 +665,14 @@ function setCities(selectedCity)
           </div>
           <div className="search-field-icons">
            
-              <img
-                className="icon-search"
-                loading="eager"
-                alt=""
-                src={require('../assets/search.png')}
-              />
-             <input placeholder="Що будемо шукати?"  type="search"  className="search-field">
-            </input>
+          <div class="input-group">
+                <span class="input-group-text" id="basic-addon1">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"></path>
+</svg>
+                </span>
+                <input type="text" class="form-control" placeholder="Що будемо шукати? " aria-label="Input group example" aria-describedby="basic-addon1"/>
+              </div>
             <div className="icons">
            
 
