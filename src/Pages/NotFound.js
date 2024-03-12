b3583c7677f614e76840b9b298b39afcb7ad1617
Yana <95874369+YaninaDol@users.noreply.github.com>
2024-03-12 23:38:24 +0400

    not f

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const NotFound = () => {
   return (
-    <div>
+    <div className='text-center'>
       <h2>404 - Page Not Found</h2>
       <p>Sorry, the requested page does not exist.</p>
-      {/* You can add additional content, styling, or links here */}
+     
     </div>
   );
 };
